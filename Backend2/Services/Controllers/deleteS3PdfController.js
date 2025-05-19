fc4bf54c751d6aa0e5bc5266f919ae17b2541128
abhi311202 <abhinitnaware01@gmail.com>
2025-05-19 18:28:10 +0530

    Minor Change in Delete pdf from S3 API - (Abhi)

--- Backend2/Services/Controllers/deleteS3PdfController.js
+++ Backend2/Services/Controllers/deleteS3PdfController.js
@@ -3,9 +3,13 @@ import { deletePdfFromS3 } from "../Models/deletePdfS3Model.js";
 export const deleteS3PdfController = async (req, res) => {
   const { fileKey } = req.body;
   if (!fileKey) return res.status(400).json({ error: "fileKey is required" });
+  if (fileKey === "key")
+    return res
+      .status(200)
+      .json({ message: "File deleted successfully", fileKey });
   try {
     const result = await deletePdfFromS3(fileKey);
-    res.json(result);
+    res.status(200).json(result);
   } catch (error) {
     res.status(500).json({ error: error.message });
   }
