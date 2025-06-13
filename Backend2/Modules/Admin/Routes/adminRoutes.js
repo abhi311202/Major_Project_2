106404bbe3b7007f7ad43bcdcc5064ccc613db07
abhi311202 <abhinitnaware01@gmail.com>
2025-06-13 16:50:36 +0530

    Admin Change email API made - (abhi)

--- Backend2/Modules/Admin/Routes/adminRoutes.js
+++ Backend2/Modules/Admin/Routes/adminRoutes.js
@@ -9,6 +9,7 @@ import { getAdminDetails } from "../Controllers/getAdminDetailsController.js";
 import { changeAdminPersonalDetailController } from "../Controllers/changeAdminPersonalDetailController.js";
 import { passwordChangeController } from "../Controllers/passwordChangeController.js";
 import { changePhoneController } from "../Controllers/changePhoneController.js";
+import { changeEmailController } from "../Controllers/changeAdminEmailController.js";
 
 router.post("/register", registerAdmin);
 router.post("/login", adminLogin);
@@ -18,7 +19,7 @@ router.post("/get-admin-details", getAdminDetails);
 router.post("/change-personal-detail", changeAdminPersonalDetailController);
 router.post("/change-password", passwordChangeController);
 router.post("/change-phone", changePhoneController);
-// router.post("/change-email", changeEmailController);
+router.post("/change-email", changeEmailController);
 // router.post("/verify", userMiddleware, demo);
 
 export default router;
