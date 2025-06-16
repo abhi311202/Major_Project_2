a2267c567b1084f58b0aceaadec9848116e4c77c
abhi311202 <abhinitnaware01@gmail.com>
2025-06-16 14:20:11 +0530

    Chat Title added as reciever_name, reciever_username-  in both user and Admin

--- Backend2/Modules/Admin/Models/getChatsFromDatabaseModel.js
+++ Backend2/Modules/Admin/Models/getChatsFromDatabaseModel.js
@@ -2,11 +2,23 @@ import client from "../../../config/sqlDB.js";
 export async function getChatsFromDatabase(admin_id) {
   console.log("In Model: " + admin_id);
   try {
-    const query = 'SELECT * FROM "chat" WHERE party2_admin_id = $1';
+    // const query = 'SELECT * FROM "chat" WHERE party2_admin_id = $1';
+    const query = `SELECT
+    c.*,
+    s.name AS receiver_name,
+    s.username AS receiver_username,
+    s.is_active AS receiver_is_active
+FROM
+    chat c
+JOIN
+    super_user s ON c.party1_superuser_id = s.id
+WHERE
+    c.party2_admin_id = $1 AND s.is_active = TRUE;`;
     const result = await client.query(query, [admin_id]);
     if (result.rows.length === 0) {
       return null;
     } else {
+      console.log(result.rows);
       return result.rows;
     }
   } catch (error) {
--- Backend2/Modules/User/Models/getChatsFromDatabaseModel.js
+++ Backend2/Modules/User/Models/getChatsFromDatabaseModel.js
@@ -2,11 +2,23 @@ import client from "../../../config/sqlDB.js";
 export async function getChatsFromDatabase(super_user_id) {
   console.log("In Model: " + super_user_id);
   try {
-    const query = 'SELECT * FROM "chat" WHERE party1_superuser_id = $1';
+    // const query = 'SELECT * FROM "chat" WHERE party1_superuser_id = $1';
+    const query = `SELECT
+    c.*,
+    a.name AS reciever_name,
+    a.username AS reciever_username,
+    a.is_active AS reciever_is_active
+FROM
+    chat c
+JOIN
+    admin a ON c.party2_admin_id = a.id
+WHERE
+     c.party1_superuser_id = $1 AND a.is_active = TRUE;`;
     const result = await client.query(query, [super_user_id]);
     if (result.rows.length === 0) {
       return null;
     } else {
+      console.log(result.rows);
       return result.rows;
     }
   } catch (error) {
