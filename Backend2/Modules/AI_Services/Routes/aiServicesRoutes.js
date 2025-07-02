001d68cc18a9cb66c3720dfb94e9034eb6ee7e92
abhi311202 <abhinitnaware01@gmail.com>
2025-07-02 15:29:10 +0530

    Document Classification Routes API Done- (abhi)

--- Backend2/Modules/AI_Services/Routes/aiServicesRoutes.js
+++ Backend2/Modules/AI_Services/Routes/aiServicesRoutes.js
@@ -4,8 +4,10 @@ const router = express.Router();
 // import { DocUpload } from "../Controllers/docUploadController.js";
 import { docScopeValidation } from "../Controllers/docScopeValidationController.js";
 import { entityMetadataExtraction } from "../Controllers/entityMetadataExtractionController.js";
+import { docClassification } from "../Controllers/docClassificationController.js";
 // router.post("/register", registerAdmin);
 router.post("/document-scope-validation", docScopeValidation);
 router.post("/entity-metadata-extraction", entityMetadataExtraction);
+router.post("/classification", docClassification);
 
 export default router;
