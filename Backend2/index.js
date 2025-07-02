ee896cf90025e52afc31da9345582ca9e233b5a3
abhi311202 <abhinitnaware01@gmail.com>
2025-07-03 00:05:39 +0530

    json payload error resolved - (abhi)

--- Backend2/index.js
+++ Backend2/index.js
@@ -13,9 +13,9 @@ import connectMongo from "./config/mongoClient.js";
 
 const app = express();
 app.use(cors());
-app.use(express.json());
 app.use(cookieParser());
-app.use(express.urlencoded({ extended: true }));
+app.use(express.json({ limit: "50mb" }));
+app.use(express.urlencoded({ extended: true, limit: "50mb" }));
 
 dotenv.config();
 const port = process.env.PORT || 4000;
